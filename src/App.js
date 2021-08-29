2daa059a69319989ea96802bd2a8420ea279031a
AfraemAhsan <afraemahsan24@gmail.com>
2021-08-29 16:11:11 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+function App() {
+  const [questions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>question and answer</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+          https://github.com/AfraemAhsan/makeportfolio.git
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
